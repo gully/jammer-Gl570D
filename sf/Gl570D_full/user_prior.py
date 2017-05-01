92338d229f39306d6fea7c0df52b2b2c3ab76aed
gully <igully@gmail.com>
2017-05-01 12:29:05 -0700

    Starfish config and priors for the Full sampling SpeX Prism spectrum, relates to Issue #12

---
+++ sf/Gl570D_full/user_prior.py
@@ -0,0 +1,15 @@
+def user_defined_lnprior(p):
+    '''
+    Takes a vector of stellar parameters and returns the ln prior.
+    '''
+    if not ((p[8] > 0) and 
+    		(p[10] > 500) and (p[10] < 5000) and
+    		(p[9] > -19) and (p[9] < -17)):
+    	return -np.inf
+
+    lnp_sigamp = -(p[8] - 1.0)**2/(2.0*0.05**2)  
+
+    ln_prior_out = lnp_sigamp
+
+    return ln_prior_out
+
